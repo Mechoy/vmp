d9c38b9e7e85a452a760b19e6440f16abac21f89
Mechoy <mechoy@yeah.net>
2024-12-17 13:06:20 +0800

    first submit

---
+++ src/main/java/com/mechoy/vmp/VmpApplication.java
@@ -0,0 +1,13 @@
+package com.mechoy.vmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmpApplication.class, args);
+    }
+
+}
